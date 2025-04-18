e492116fa89756931c6e5d295642c0e4f3b7f4fc
Rakib <smrakib.cse.pust@gmail.com>
2025-04-18 21:56:14 +0600

    Add files via upload

---
+++ sum of the first 10 natural numbers.py
@@ -0,0 +1,8 @@
+# Sum of first 10 natural numbers using a for loop
+
+total = 0
+
+for i in range(1, 11):  # Loop from 1 to 10 (inclusive)
+    total += i
+
+print("Sum of the first 10 natural numbers is:", total)
